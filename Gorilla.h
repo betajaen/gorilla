256486f0aa6404fedd7b2f962d9dc14c8b4d436e
Nigel Atkinson <nigel@atkinson.gen.nz>
2011-04-12 14:01:39 +1000

    Add header changes.

--- Gorilla.h
+++ Gorilla.h
@@ -342,13 +342,13 @@ namespace Gorilla
    
   public:
    
-   Glyph() : uvTop(0), uvBottom(0), uvWidth(0), uvHeight(0), uvLeft(0), uvRight(0), glyphWidth(0), glyphHeight(0), glyphAdvance(0) {}
+   Glyph() : uvTop(0), uvBottom(0), uvWidth(0), uvHeight(0), uvLeft(0), uvRight(0), glyphWidth(0), glyphHeight(0), glyphAdvance(0), verticalOffset(0) {}
    
   ~Glyph() {}
    
    Ogre::Vector2    texCoords[4];
    Ogre::Real uvTop, uvBottom, uvWidth, uvHeight, uvLeft, uvRight,
-                       glyphWidth, glyphHeight, glyphAdvance;
+                       glyphWidth, glyphHeight, glyphAdvance, verticalOffset;
    buffer<Kerning> kerning;
    
    // Get kerning value of a character to the right of another.
@@ -695,6 +695,7 @@ namespace Gorilla
     void  _loadTexture(Ogre::ConfigFile::SettingsMultiMap*);
     void  _loadGlyphs(Ogre::ConfigFile::SettingsMultiMap*, GlyphData*);
     void  _loadKerning(Ogre::ConfigFile::SettingsMultiMap*, GlyphData*);
+    void  _loadVerticalOffsets(Ogre::ConfigFile::SettingsMultiMap*, GlyphData*);
     void  _loadSprites(Ogre::ConfigFile::SettingsMultiMap*);
     void  _create2DMaterial();
     void  _create3DMaterial();
@@ -2810,6 +2811,7 @@ namespace Gorilla
     {
      mLeft = left;
      mDirty = true;
+     mTextDirty = true;
      mLayer->_markDirty();
     }
 
@@ -2830,6 +2832,7 @@ namespace Gorilla
     {
      mTop = top;
      mDirty = true;
+     mTextDirty = true;
      mLayer->_markDirty();
     }
     
@@ -2886,6 +2889,16 @@ namespace Gorilla
      mLayer->_markDirty();
     }
     
+    /*! function. maxTextWidth
+        desc.
+            Get the maximum width of the text can draw into.
+    */
+    Ogre::Real maxTextWidth()
+    {
+     _calculateCharacters();
+     return mMaxTextWidth;
+    }
+
     /*! function. caption
         desc.
             Get the text indented to show.
@@ -2970,6 +2983,7 @@ namespace Gorilla
     Layer*                mLayer;
     GlyphData*            mDefaultGlyphData;
     Ogre::Real            mLeft, mTop, mWidth, mHeight;
+    Ogre::Real            mMaxTextWidth;
     Ogre::String          mText;
     Ogre::ColourValue     mBackground;
     bool                  mDirty, mTextDirty;
